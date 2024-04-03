e529794456d7bf11ff1878635b92a2c8f87ae6b6
KevynSantos <52923238+KevynSantos@users.noreply.github.com>
2024-04-02 22:33:34 -0300

    Incluindo arquivos da pasta .venv

---
+++ .venv/Lib/run.py
@@ -0,0 +1,30 @@
+import os
+from jsmin import jsmin
+from jproperties import Properties
+
+configs = Properties()
+
+with (open('config.properties', 'rb') as config_file):
+    configs.load(config_file)
+    # Diretório onde estão os arquivos .js
+    directory = configs.get("path").data
+
+    # Listar todos os arquivos .js no diretório
+    js_files = [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith(".js")]
+
+    try:
+        # Para cada arquivo .js, minificar e sobrescrever o arquivo
+        for js_file in js_files:
+            with open(js_file, "r", encoding="utf-8") as file:
+                original_code = file.read()
+
+            # Minificar o código JavaScript
+            minified_code = jsmin(original_code)
+
+            # Sobrescrever o arquivo com o código minificado
+            with open(js_file, "w", encoding="utf-8") as file:
+                file.write(minified_code)
+
+        print("Sucesso")
+    except Exception as e:
+        print(f"Falha ao minificar os arquivos JavaScript: {e}")
